8a71e0761c29f07e120c13d1c821516cf70da4a5
James Kontargyris <james@jameskontargyris.co.uk>
2016-08-05 15:49:50 +0000

    Updated page-section to use direction-context mixin

--- wp-content/themes/happyduck/template-parts/content-page-2-col-sidebar-left.php
+++ wp-content/themes/happyduck/template-parts/content-page-2-col-sidebar-left.php
@@ -1,9 +1,9 @@
 <div class="page-section">
-	<div class="page-section__container">
-		<div class="page-section__main--shift-right">
+	<div class="page-section__container page-section__container--reverse">
+		<div class="page-section__main">
 			<?php the_content(); ?>
 		</div>
-		<div class="page-section__sidebar--shift-left">
+		<div class="page-section__sidebar">
 			<?php get_sidebar(); ?>
 		</div>
 	</div>
