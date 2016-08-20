7d51cc49dc8cb398d1f698f99a207a4bd5357b98
James Kontargyris <james@jameskontargyris.co.uk>
2016-08-20 19:44:07 +0100

    Signup form widget updates

--- wp-content/plugins/happyduck-plugin/inc/mailinglist_signup_form_widget.php
+++ wp-content/plugins/happyduck-plugin/inc/mailinglist_signup_form_widget.php
@@ -28,18 +28,18 @@ class Mailinglist_Signup_Form_Widget extends WP_Widget {
 //		TODO: update form to point at correct page
 		?>
 
-		<div class="signup-form signup-form--padded signup-form--rounded-corners <?php echo $additional_classes; ?>">
-			<div class="signup-form__intro text--white">
-				<h4 class="text--yellow text--bold margin--none"><?php echo $instance['title']; ?></h4>
+		<div class="signup-form signup-form--padded signup-form--rounded-corners <?php echo $additional_classes; if($instance['colour'] == 'blue') : ?> signup-form--blue<?php endif; ?>">
+			<div class="signup-form__intro">
+				<h4 class="signup-form__title text--bold margin--none"><?php echo $instance['title']; ?></h4>
 				<?php echo $instance['intro']; ?>
 			</div>
 			<div class="signup-form__fields">
 				<div class="signup-form__fieldgroup">
-					<label class="text--white" for="name">Your name</label>
+					<label for="name">Your name</label>
 					<input type="text" class="input--full-span">
 				</div>
 				<div class="signup-form__fieldgroup">
-					<label class="text--white" for="email">Your email address</label>
+					<label for="email">Your email address</label>
 					<input type="text" class="input--full-span">
 				</div>
 				<div class="signup-form__fieldgroup">
@@ -63,6 +63,7 @@ class Mailinglist_Signup_Form_Widget extends WP_Widget {
 		$title = ! empty( $instance['title'] ) ? $instance['title'] : __( 'Join our Mailing List', 'happyduck' );
 		$intro = ! empty( $instance['intro'] ) ? $instance['intro'] : __( 'Receive tips, articles and advice direct from HDHQ to your inbox.', 'happyduck' );
 		$direction = ! empty( $instance['direction'] ) ? $instance['direction'] : __( '', 'happyduck' );
+		$colour = ! empty( $instance['colour'] ) ? $instance['colour'] : 'grey';
 		$classes = ! empty( $instance['classes'] ) ? $instance['classes'] : __( '', 'happyduck' );
 		?>
 		<p>
@@ -78,6 +79,13 @@ class Mailinglist_Signup_Form_Widget extends WP_Widget {
 			<select id="<?php echo $this->get_field_id( 'direction' ); ?>" name="<?php echo $this->get_field_name( 'direction' ); ?>">
 				<option value="vertical" <?php if($direction == 'vertical') : ?>selected<?php endif; ?>>Vertical</option>
 				<option value="horizontal" <?php if($direction == 'horizontal') : ?>selected<?php endif; ?>>Horizontal</option>
+			</select>
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'colour' ); ?>"><?php _e( 'Colour:' ); ?></label>
+			<select id="<?php echo $this->get_field_id( 'colour' ); ?>" name="<?php echo $this->get_field_name( 'colour' ); ?>">
+				<option value="grey" <?php if($colour == 'grey') : ?>selected<?php endif; ?>>Grey</option>
+				<option value="blue" <?php if($colour == 'blue') : ?>selected<?php endif; ?>>Blue</option>
 			</select>
 		</p>
 		<p>
@@ -100,6 +108,7 @@ class Mailinglist_Signup_Form_Widget extends WP_Widget {
 		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
 		$instance['intro'] = ( ! empty( $new_instance['intro'] ) ) ? strip_tags( $new_instance['intro'] ) : '';
 		$instance['direction'] = ( ! empty( $new_instance['direction'] ) ) ? strip_tags( $new_instance['direction'] ) : '';
+		$instance['colour'] = ( ! empty( $new_instance['colour'] ) ) ? strip_tags( $new_instance['colour'] ) : '';
 		$instance['classes'] = ( ! empty( $new_instance['classes'] ) ) ? strip_tags( $new_instance['classes'] ) : '';
 
 		return $instance;
